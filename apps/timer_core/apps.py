dc524a93747683a73809a9f01208df1d0936fd41
sgupge2445 <22238287@edu.cc.saga-u.ac.jp>
2025-06-16 13:37:22 +0900

    アプリケーションのnameの修正

--- apps/timer_core/apps.py
+++ apps/timer_core/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TimerCoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'timer_core'
+    name = 'apps.timer_core'
