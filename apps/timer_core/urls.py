c8ef01ccc657ccfc63799ad6b62b8c47b80e9700
sgupge2445 <22238287@edu.cc.saga-u.ac.jp>
2025-09-27 17:56:15 +0900

    タイマー画面がメイン画面になるように変更

--- apps/timer_core/urls.py
+++ apps/timer_core/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'timer_core'
 
 urlpatterns = [
-    path('', views.band_list, name='band_list'),           # バンド一覧（トップページ）
-    path('band/<int:band_id>/', views.timer_list, name='timer_list'),  # タイマー一覧
+    path('', views.timer_main, name='timer_main'),
 ]
--- apps/timer_core/views.py
+++ apps/timer_core/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Band, Timer
 
-def band_list(request):
-    """バンド一覧画面"""
-    bands = Band.objects.all()
-    return render(request, 'timer_core/band_list.html', {
-        'bands': bands
-    })
+def timer_main(request):
+    """メインタイマー画面"""
+    timers = Timer.objects.filter(is_active=True).select_related('band').order_by('order')
 
-def timer_list(request, band_id):
-    """タイマー一覧画面"""
-    band = get_object_or_404(Band, id=band_id)
-    timers = Timer.objects.filter(band=band, is_active=True).order_by('order')
-    
-    return render(request, 'timer_core/timer_list.html', {
-        'band': band,
+    return render(request, 'timer_core/timer_main.html', {
         'timers': timers
     })
