4591c77302fdf7ad59d22dab93c85ac1d4c7f7b9
sgupge2445 <22238287@edu.cc.saga-u.ac.jp>
2025-09-27 15:22:25 +0900

    ビューを作成

--- apps/timer_core/views.py
+++ apps/timer_core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Band, Timer
 
-# Create your views here.
+def band_list(request):
+    """バンド一覧画面"""
+    bands = Band.objects.all()
+    return render(request, 'timer_core/band_list.html', {
+        'bands': bands
+    })
+
+def timer_list(request, band_id):
+    """タイマー一覧画面"""
+    band = get_object_or_404(Band, id=band_id)
+    timers = Timer.objects.filter(band=band, is_active=True).order_by('order')
+    
+    return render(request, 'timer_core/timer_list.html', {
+        'band': band,
+        'timers': timers
+    })
