47eb8dbfb983286c4fd981f84190d4a806b4c15e
sgupge2445 <22238287@edu.cc.saga-u.ac.jp>
2025-09-27 18:26:58 +0900

    APIのurlを追加

--- apps/timer_core/urls.py
+++ apps/timer_core/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'timer_core'
 
 urlpatterns = [
-    path('', views.timer_main, name='timer_main'),
+    path('', views.timer_main, name='timer_main'),         # タイマーメイン画面（トップページ）
+    path('api/start-timer/', views.start_timer, name='start_timer'),    # タイマー開始API
+    path('api/timer-status/<str:session_id>/', views.timer_status, name='timer_status'),  # タイマー状態API
 ]
