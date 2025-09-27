8cf24e0825ac10fe771c3cba4e589231aef8c6eb
sgupge2445 <22238287@edu.cc.saga-u.ac.jp>
2025-09-27 16:14:01 +0900

    urlの記述の修正

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('timer/', include('apps.timer_core.urls')),
+    path('', include('apps.timer_core.urls')),
 ]
