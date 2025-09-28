29e5215bde5869ef13a77cd74c06055e01ef77e1
sgupge2445 <22238287@edu.cc.saga-u.ac.jp>
2025-09-28 09:18:46 +0900

    ASGI設定ファイルの作成

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,30 @@
 """
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+ASGI config for band rehearsal timer project.
+WebSocket + HTTP 対応
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
-application = get_asgi_application()
+# Django ASGI アプリケーション初期化
+django_asgi_app = get_asgi_application()
+
+# WebSocket URL routing
+from apps.timer_core.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    # HTTP リクエスト処理
+    "http": django_asgi_app,
+    
+    # WebSocket リクエスト処理
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
+})
